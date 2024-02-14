a81eb713ec6037ead95e9c6ec5bc5ebfbc02c27f
SammyEc <samuelto1359@gmail.com>
2024-02-14 00:12:47 -0500

    cors

--- config/corsOptions.mjs
+++ config/corsOptions.mjs
@@ -1,7 +1,8 @@
 const whitelist = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'http://52.22.2.130'
 ];
 
 const corsOptions = {
