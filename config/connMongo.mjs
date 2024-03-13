442ed5d3fbfc574baf4196e63a7af47b4967f097
SammyEc <samuelto1359@gmail.com>
2024-03-13 05:48:47 -0500

    conexion mongo

--- config/connMongo.mjs
+++ config/connMongo.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 try {
   // Connect to MongoDB - Configurar valor de variable de entorno en archivo .env
   //console.log(process.env.MONGO_URL_LOCAL);MONGO_URL_ATLAS2)
-  await mongoose.connect(process.env.MONGO_URL_LOCAL)
+  await mongoose.connect(process.env.MONGO_URL_ATLAS2)
                 .then(() => console.log("DB funcionando"))
                 .catch((error) => console.error(error));
                 
